7c1dfcbb12682c79c028beca0473a842a055dd0f
Cody Mailey <cmailey03@gmail.com>
2019-10-08 10:11:15 -0600

    bigger than done

--- list_quiz.rb
+++ list_quiz.rb
@@ -18,3 +18,31 @@ end
 # puts three_even([2, 1, 64, 56, 71, 871, 23423, 43541, 456541, 4561 ,451,561 , 51])
 # puts three_even([])
 
+# _______________________________________________
+def bigger_than(first, second)
+    first_sum = 0
+    second_sum = 0
+    i = 0
+    first.size.times do
+        first_sum = first_sum + first[i]
+        i = i + 1
+    end
+    i = 0
+    second.size.times do |amount|
+        second_sum = second_sum + second[i]
+        i = i + 1
+    end
+    if second_sum > first_sum
+        return second
+    elsif first_sum > second_sum
+        return first
+    elsif first_sum == second_sum
+        return first
+    end
+end
+
+# print bigger_than([1, 2], [3, 4])
+# print bigger_than([1, 7], [4, 4])
+
+# _______________________________________________
+
