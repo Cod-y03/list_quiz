031155da37c9763a1dfe19fd1c5e0a2cb655e46a
Cody Mailey <cmailey03@gmail.com>
2019-10-08 10:24:24 -0600

    chage three even

--- list_quiz.rb
+++ list_quiz.rb
@@ -11,12 +11,11 @@ def three_even(list)
     return false
 end
 
-# puts three_even([2, 1, 3, 5])#false
-# puts three_even([2, 4, 12, 5]) #true
-# puts three_even([2, 4, 1, 6]) #false
-# puts three_even([2, 4, 4, 6]) #true
-# puts three_even([2, 1, 64, 56, 71, 871, 23423, 43541, 456541, 4561 ,451,561 , 51])
-# puts three_even([])
+puts three_even([2, 1, 3, 5])#false
+puts three_even([2, 4, 12, 5]) #true
+puts three_even([2, 4, 4, 6]) #true
+puts three_even([2, 1, 64, 56, 71, 871, 23423, 43541, 456541, 4561 ,451,561 , 51])
+puts three_even([])
 
 # _______________________________________________
 def bigger_than(first, second)
